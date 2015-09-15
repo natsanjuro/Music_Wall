c2dbf8e500db554e6100a840744bbc3c531da9a5
vagrant <natsanjuro@gmail.com>
2015-09-15 16:41:28 +0000

    first commit

---
+++ app/actions.rb
@@ -0,0 +1,32 @@
+# Homepage (Root path)
+get '/' do
+  erb :index
+end
+
+get '/songs' do
+  @songs = Song.all
+  erb :'songs/index'
+end
+
+get '/songs/new' do
+  @song = Song.new
+  erb :'songs/new'
+end
+
+post '/songs' do
+  @song = Song.new(
+    title: params[:title],
+    url: params[:url],
+    artist:  params[:artist]
+   )
+  if @song.save
+    redirect '/songs'
+  else
+    erb :'songs/new'
+  end
+end
+
+get '/songs/:id' do
+  @song = Song.find params[:id]
+  erb :'songs/show'
+end
